aac5d3da85c2142dcebdd45d32dfdb2aa8599cff
Zagir <zainudinov.zagir@gmail.com>
2024-01-01 06:21:19 +0300

    file structure has been changed

--- api_requests/request.py
+++ api_requests/request.py
@@ -2,10 +2,10 @@ import json
 
 import requests
 
-import config
+from settings import api_config
 
 def get_city_coord(city):
-    payload = {'geocode': city, 'apikey': config.geo_key, 'format': 'json'}
+    payload = {'geocode': city, 'apikey': api_config.geo_key, 'format': 'json'}
     r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
     geo = json.loads(r.text)
     return geo['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
@@ -14,7 +14,7 @@ def get_city_coord(city):
 def get_weather(city):
     coordinates = get_city_coord(city).split()
     payload = {'lat': coordinates[1], 'lon': coordinates[0], 'lang': 'ru_RU'}
-    r = requests.get('https://api.weather.yandex.ru/v2/forecast', params=payload, headers=config.weather_key)
+    r = requests.get('https://api.weather.yandex.ru/v2/forecast', params=payload, headers=api_config.weather_key)
     weather_data = json.loads(r.text)
     return weather_data
 
